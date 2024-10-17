db5bc0da9c40d62bb4834aa559f0104866ecf816
Gabrielle <gabriellearrudac@gmail.com>
2024-10-17 17:33:53 -0300

    muda arquivos para py

---
+++ selecao-usuarios/passo_2_match_com_equipe_e_upload_turn.py
@@ -0,0 +1,27 @@
+### Match com equipe e Upload na Turn
+# A partir dos usuários selecionados para o dia no Passo 1, une suas informações com suas informações 
+# de equipe/estabelecimentos que esta no Big Query e advem da Turn. Após isso da upload desses contatos na turn.
+#### Configurações iniciais do ambiente
+
+
+
+#### Match
+# Une dados da tabela de seção diaria "ip_mensageria_camada_prata.historico_envio_mensagens" do BigQuery 
+# com os dados do estabelecimento que a pessoa é pertencente, por meio do dos dados que estão registradas 
+# na tabela "5511974565221.contacts" no BigQuery.
+
+
+
+# #### Upload
+# Da upload no perfil respectivo ao seu municipio na Turn. Inclui os seguintes campos:
+# - opted_in=true
+# - 
+tokens_municipios = [
+    {"municipio": "Paulo Ramos", "id_sus": "210810", "token": os.getenv('ENV_PAULORAMOS_MA')},
+    {"municipio": "Pacoti", "id_sus": "210810", "token": os.getenv('ENV_PACOTI_CE')},
+    {"municipio": "Marajá do Sena", "id_sus": "210810", "token": os.getenv('ENV_MARAJADOSENA_MA')},
+    {"municipio": "Monsenhor Tabosa", "id_sus": "210810", "token": os.getenv('ENV_MONSENHORTABOSA_CE')},
+    {"municipio": "Lago Verde", "id_sus": "210590", "token": os.getenv('ENV_LAGOVERDE_MA')}                                        
+]
+for linha in dataframe:
+    
---
+++ selecao-usuarios/passo_3_envio_de_mensagens.py
@@ -0,0 +1,7 @@
+# ### Envio de mensagens
+
+# A partir dos usuários que foram selecionados e enriquecidos os dados envia programa seu envio de mensagem.
+
+# #### Configurações iniciais do ambiente
+
+# #### Programa a mensagem
