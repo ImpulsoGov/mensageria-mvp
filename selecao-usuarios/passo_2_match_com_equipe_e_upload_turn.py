958c733dcd5c1380dd95bcfb923ba244cff54b04
João Vittor Souza <jvns2010@gmail.com>
2024-10-21 16:53:40 -0300

    add comentario com os campos dos usuarios

--- selecao-usuarios/passo_2_match_com_equipe_e_upload_turn.py
+++ selecao-usuarios/passo_2_match_com_equipe_e_upload_turn.py
@@ -17,14 +17,34 @@
 # #### Upload
 # Da upload no perfil respectivo ao seu municipio na Turn. Inclui os seguintes campos:
 # - opted_in=true
-# - 
+# - nome_do_paciente
+# - linha_de_cuidado
+# - municipio
+# - municipio_id_sus
+# - equipe_ine
+# - equipe_nome
+# - mvp_tipo_grupo
+# - mvp_data_envio
+# - mvp_grupo
+# - estabelecimento_horario
+# - estabelecimento_documentos
+# - estabelecimento_nome
+# - estabelecimento_endereco
+# - estabelecimento_telefone
+# - horarios_cronicos
+# - horarios_cito
 tokens_municipios = [
     {"municipio": "Paulo Ramos", "id_sus": "210810", "token": os.getenv('ENV_PAULORAMOS_MA')},
     {"municipio": "Pacoti", "id_sus": "210810", "token": os.getenv('ENV_PACOTI_CE')},
     {"municipio": "Marajá do Sena", "id_sus": "210810", "token": os.getenv('ENV_MARAJADOSENA_MA')},
     {"municipio": "Monsenhor Tabosa", "id_sus": "210810", "token": os.getenv('ENV_MONSENHORTABOSA_CE')},
-    {"municipio": "Lago Verde", "id_sus": "210590", "token": os.getenv('ENV_LAGOVERDE_MA')}                                        
+    {"municipio": "Lago Verde", "id_sus": "210590", "token": os.getenv('ENV_LAGOVERDE_MA')},
+    {"municipio": "Alagoinha", "id_sus": "260060", "token": os.getenv('ENV_ALAGOINHA_PE')},
+    {"municipio": "Baraúna", "id_sus": "240145", "token": os.getenv('ENV_BARAUNA_RN')},
+    {"municipio": "Jucuruçu", "id_sus": "291845", "token": os.getenv('ENV_JUCURUCU_BA')},
+    {"municipio": "Vitorino Freire", "id_sus": "211300", "token": os.getenv('ENV_VITORINOFREIRE_MA')},
+
 ]
 
 print("Oie")
-    
+
