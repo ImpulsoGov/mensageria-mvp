d50b02b494bea2169a26ad4021557d1749d4572c
Alexsandro Francisco <alexsandro2134@gmail.com>
2024-10-29 11:47:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def passo3():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
