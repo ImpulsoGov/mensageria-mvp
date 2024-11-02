401a3ba84f68d914476d0bad19eb51812d1a9fca
Gabrielle <gabriellearrudac@gmail.com>
2024-11-02 19:58:28 -0300

    atualiza token e template

--- src/scripts/passo_3_envio_de_mensagens.py
+++ src/scripts/passo_3_envio_de_mensagens.py
@@ -18,22 +18,21 @@ from src.bd import BigQueryClient
 from src.loggers import logger
 
 
-
 tokens_municipios = [
-    {"municipio": "Paulo Ramos", "id_sus": "210810", "token": os.getenv('ENV_PAULORAMOS_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Lago Verde", "id_sus": "210590", "token": os.getenv('ENV_LAGOVERDE_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Pacoti", "id_sus": "230980", "token": os.getenv('ENV_PACOTI_CE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Monsenhor Tabosa", "id_sus": "230860", "token": os.getenv('ENV_MONSENHORTABOSA_CE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Marajá do Sena", "id_sus": "210635", "token": os.getenv('ENV_MARAJADOSENA_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Alagoinha", "id_sus": "260060", "token": os.getenv('ENV_ALAGOINHA_PE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Baraúna", "id_sus": "240145", "token": os.getenv('ENV_BARAUNA_RN'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Jucuruçu", "id_sus": "291845", "token": os.getenv('ENV_JUCURUCU_BA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Vitorino Freire", "id_sus": "211300", "token": os.getenv('ENV_VITORINOFREIRE_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Brejo de Areia", "id_sus": "210215", "token": os.getenv('ENV_BREJODEAREIA_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Oiapoque", "id_sus": "160050", "token": os.getenv('ENV_OIAPOQUE_AP'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Tarrafas", "id_sus": "231325", "token": os.getenv('ENV_TARRAFAS_CE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Salvaterra", "id_sus": "150630", "token": os.getenv('ENV_SALVATERRA_PA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Lagoa do Ouro", "id_sus": "260860", "token": os.getenv('ENV_LAGOADOOURO_PE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
+    {"municipio": "Paulo Ramos", "id_sus": "210810", "token": os.getenv('ENV_PAULORAMOS_MA'), "link_imagem": "https://i.imgur.com/zn7sRTV.png"},
+    {"municipio": "Lago Verde", "id_sus": "210590", "token": os.getenv('ENV_LAGOVERDE_MA'), "link_imagem": "https://i.imgur.com/AzH0p2d.png"},
+    {"municipio": "Pacoti", "id_sus": "230980", "token": os.getenv('ENV_PACOTI_CE'), "link_imagem": "https://i.imgur.com/TWkhHBq.png"},
+    {"municipio": "Monsenhor Tabosa", "id_sus": "230860", "token": os.getenv('ENV_MONSENHORTABOSA_CE'), "link_imagem": "https://i.imgur.com/5r2R0vz.png"},
+    {"municipio": "Marajá do Sena", "id_sus": "210635", "token": os.getenv('ENV_MARAJADOSENA_MA'), "link_imagem": "https://i.imgur.com/PHEjjwW.png"},
+    {"municipio": "Alagoinha", "id_sus": "260060", "token": os.getenv('ENV_ALAGOINHA_PE'), "link_imagem": "https://i.imgur.com/Hn7Z9TI.png"},
+    {"municipio": "Baraúna", "id_sus": "240145", "token": os.getenv('ENV_BARAUNA_RN'), "link_imagem": "https://i.imgur.com/aQDjqJv.png"},
+    {"municipio": "Jucuruçu", "id_sus": "291845", "token": os.getenv('ENV_JUCURUCU_BA'), "link_imagem": "https://i.imgur.com/Rp2zq72.png"},
+    {"municipio": "Vitorino Freire", "id_sus": "211300", "token": os.getenv('ENV_VITORINOFREIRE_MA'), "link_imagem": "https://i.imgur.com/BmqRuY0.png"},
+    {"municipio": "Brejo de Areia", "id_sus": "210215", "token": os.getenv('ENV_BREJODEAREIA_MA'), "link_imagem": "https://i.imgur.com/tO0qHav.png"},
+    {"municipio": "Oiapoque", "id_sus": "160050", "token": os.getenv('ENV_OIAPOQUE_AP'), "link_imagem": "https://i.imgur.com/ZlHJHcR.png"},
+    {"municipio": "Tarrafas", "id_sus": "231325", "token": os.getenv('ENV_TARRAFAS_CE'), "link_imagem": "https://i.imgur.com/J93IDWu.png"},
+    {"municipio": "Salvaterra", "id_sus": "150630", "token": os.getenv('ENV_SALVATERRA_PA'), "link_imagem": "https://i.imgur.com/bnwogCJ.png"},
+    {"municipio": "Lagoa do Ouro", "id_sus": "260860", "token": os.getenv('ENV_LAGOADOOURO_PE'), "link_imagem": "https://i.imgur.com/xGLsHnO.png"},
 ]
 URL_API_MENSAGENS = "https://whatsapp.turn.io/v1/messages"
 TEMPLATE_NAMESPACE = os.getenv('TEMPLATE_NAMESPACE')
@@ -41,7 +40,7 @@ TEMPLATE_NAMESPACE = os.getenv('TEMPLATE_NAMESPACE')
 #### Funcoes
 def seleciona_template_por_linha_de_cuidado(contato):
     nome_template_cito = "mensageria_usuarios_campanha_citopatologico_v01"
-    nome_template_cronicos = "mensageria_usuarios_campanha_cronicos_v0"
+    nome_template_cronicos = "mensageria_usuarios_campanha_cronicos_v1"
     if contato['linha_cuidado'] == "citopatologico":
         template_nome = nome_template_cito
         type_header = "image"
@@ -49,7 +48,7 @@ def seleciona_template_por_linha_de_cuidado(contato):
     elif contato['linha_cuidado'] == "cronicos":
         template_nome = nome_template_cronicos
         type_header="video"
-        link_media= "https://i.imgur.com/gHWgdG4.mp4"
+        link_media= "https://i.imgur.com/UFOsqw9.mp4"
     else:
         return None
     template = {
