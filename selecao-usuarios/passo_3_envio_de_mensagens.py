4911b7595ba40bdeacf97ee40a28fd0c1bbde4bd
Gabrielle <gabriellearrudac@gmail.com>
2024-11-11 10:57:42 -0300

    troca para bases de disparo

--- selecao-usuarios/passo_3_envio_de_mensagens.py
+++ selecao-usuarios/passo_3_envio_de_mensagens.py
@@ -22,20 +22,20 @@ project_id = 'predictive-keep-314223'
 client = bigquery.Client(credentials= credentials,project=project_id)
 
 tokens_municipios = [
-    {"municipio": "Paulo Ramos", "id_sus": "210810", "token": os.getenv('ENV_PAULORAMOS_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Lago Verde", "id_sus": "210590", "token": os.getenv('ENV_LAGOVERDE_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Pacoti", "id_sus": "230980", "token": os.getenv('ENV_PACOTI_CE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Monsenhor Tabosa", "id_sus": "230860", "token": os.getenv('ENV_MONSENHORTABOSA_CE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Marajá do Sena", "id_sus": "210635", "token": os.getenv('ENV_MARAJADOSENA_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Alagoinha", "id_sus": "260060", "token": os.getenv('ENV_ALAGOINHA_PE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Baraúna", "id_sus": "240145", "token": os.getenv('ENV_BARAUNA_RN'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Jucuruçu", "id_sus": "291845", "token": os.getenv('ENV_JUCURUCU_BA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Vitorino Freire", "id_sus": "211300", "token": os.getenv('ENV_VITORINOFREIRE_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Brejo de Areia", "id_sus": "210215", "token": os.getenv('ENV_BREJODEAREIA_MA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Oiapoque", "id_sus": "160050", "token": os.getenv('ENV_OIAPOQUE_AP'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Tarrafas", "id_sus": "231325", "token": os.getenv('ENV_TARRAFAS_CE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Salvaterra", "id_sus": "150630", "token": os.getenv('ENV_SALVATERRA_PA'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
-    {"municipio": "Lagoa do Ouro", "id_sus": "260860", "token": os.getenv('ENV_LAGOADOOURO_PE'), "link_imagem": "https://avatars.githubusercontent.com/u/63020100?s=280&v=4"},
+    {"municipio": "Paulo Ramos", "id_sus": "210810", "token": os.getenv('ENV_PAULORAMOS_MA'), "link_imagem": "https://i.imgur.com/zn7sRTV.png"},
+    {"municipio": "Lago Verde", "id_sus": "210590", "token": os.getenv('ENV_LAGOVERDE_MA'), "link_imagem": "https://i.imgur.com/AzH0p2d.png"},
+    {"municipio": "Pacoti", "id_sus": "230980", "token": os.getenv('ENV_PACOTI_CE'), "link_imagem": "https://i.imgur.com/TWkhHBq.png"},
+    {"municipio": "Monsenhor Tabosa", "id_sus": "230860", "token": os.getenv('ENV_MONSENHORTABOSA_CE'), "link_imagem": "https://i.imgur.com/5r2R0vz.png"},
+    {"municipio": "Marajá do Sena", "id_sus": "210635", "token": os.getenv('ENV_MARAJADOSENA_MA'), "link_imagem": "https://i.imgur.com/PHEjjwW.png"},
+    {"municipio": "Alagoinha", "id_sus": "260060", "token": os.getenv('ENV_ALAGOINHA_PE'), "link_imagem": "https://i.imgur.com/Hn7Z9TI.png"},
+    {"municipio": "Baraúna", "id_sus": "240145", "token": os.getenv('ENV_BARAUNA_RN'), "link_imagem": "https://i.imgur.com/aQDjqJv.png"},
+    {"municipio": "Jucuruçu", "id_sus": "291845", "token": os.getenv('ENV_JUCURUCU_BA'), "link_imagem": "https://i.imgur.com/Rp2zq72.png"},
+    # {"municipio": "Vitorino Freire", "id_sus": "211300", "token": os.getenv('ENV_VITORINOFREIRE_MA'), "link_imagem": "https://i.imgur.com/BmqRuY0.png"},
+    # {"municipio": "Brejo de Areia", "id_sus": "210215", "token": os.getenv('ENV_BREJODEAREIA_MA'), "link_imagem": "https://i.imgur.com/tO0qHav.png"},
+    # {"municipio": "Oiapoque", "id_sus": "160050", "token": os.getenv('ENV_OIAPOQUE_AP'), "link_imagem": "https://i.imgur.com/ZlHJHcR.png"},
+    {"municipio": "Tarrafas", "id_sus": "231325", "token": os.getenv('ENV_TARRAFAS_CE'), "link_imagem": "https://i.imgur.com/J93IDWu.png"},
+    {"municipio": "Salvaterra", "id_sus": "150630", "token": os.getenv('ENV_SALVATERRA_PA'), "link_imagem": "https://i.imgur.com/bnwogCJ.png"},
+    {"municipio": "Lagoa do Ouro", "id_sus": "260860", "token": os.getenv('ENV_LAGOADOOURO_PE'), "link_imagem": "https://i.imgur.com/xGLsHnO.png"},
 ]
 URL_API_MENSAGENS = "https://whatsapp.turn.io/v1/messages"
 TEMPLATE_NAMESPACE = os.getenv('TEMPLATE_NAMESPACE')
@@ -104,10 +104,10 @@ def envia_mensagem(token, contato, template):
         "template" : template
     }
     response = requests.post(URL_API_MENSAGENS, headers=headers, data=json.dumps(dados_de_envio))
-    time.sleep(1)
+    time.sleep(1.05)
 
-    # update_query = f"""UPDATE `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens` SET mvp_status_envio = "{response.status_code}" WHERE celular_tratado = "{str(contato["celular_tratado"])}" and mvp_data_envio = "{str(contato["mvp_data_envio"])}";"""
-    update_query = f"""UPDATE `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens_teste` SET mvp_status_envio = "{response.status_code}" WHERE celular_tratado = {str(contato["celular_tratado"])} and mvp_data_envio = "{str(contato["mvp_data_envio"])}" and municipio = "{contato["municipio"]}";"""
+    update_query = f"""UPDATE `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens` SET mvp_status_envio = "{response.status_code}" WHERE celular_tratado = "{str(contato["celular_tratado"])}" and mvp_data_envio = "{str(contato["mvp_data_envio"])}";"""
+    # update_query = f"""UPDATE `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens_teste` SET mvp_status_envio = "{response.status_code}" WHERE celular_tratado = {str(contato["celular_tratado"])} and mvp_data_envio = "{str(contato["mvp_data_envio"])}" and municipio = "{contato["municipio"]}";"""
     update_job = client.query(update_query)
     if response.status_code == 201 or response.status_code == 200:
         print(f"Mensagem enviada para {whatsapp_id}")
@@ -132,15 +132,15 @@ def seleciona_horario(contato):
 
 #### Consulta dos dados
 # carregando a tabela de usuários selecionados
-# query = """
-#     SELECT *
-#     FROM `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens`
-#     WHERE mvp_grupo = "teste" and mvp_data_envio = current_date("America/Sao_Paulo")
-# """
 query = """
     SELECT *
-    FROM `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens_teste`
-    WHERE mvp_grupo = "teste" and DATE(TIMESTAMP(mvp_data_envio), "America/Sao_Paulo") = DATE(current_datetime("America/Sao_Paulo"))"""
+    FROM `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens`
+    WHERE mvp_grupo = "teste" and mvp_data_envio = current_date("America/Sao_Paulo")
+"""
+# query = """
+#     SELECT *
+#     FROM `predictive-keep-314223.ip_mensageria_camada_prata.historico_envio_mensagens_teste`
+#     WHERE mvp_grupo = "teste" and DATE(TIMESTAMP(mvp_data_envio), "America/Sao_Paulo") = DATE(current_datetime("America/Sao_Paulo"))"""
 query_job = client.query(query)
 rows = [dict(row) for row in query_job]
 contatos = pd.DataFrame(rows)
